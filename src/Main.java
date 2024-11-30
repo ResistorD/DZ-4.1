2d05b6308f826b67760f83a05643843f54a06935
ResistorD <resistord@gmail.com>
2024-11-30 16:34:08 +0400

    Домашка готова

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Домашка 4.1");
+
+        System.out.println("Задача 1");
+
+        for (int i = 1; i <= 10; i++)
+            System.out.println(i);
+
+        System.out.println("Задача 2");
+
+        for (int i = 10; i >= 1; i--)
+            System.out.println(i);
+
+        System.out.println("Задача 3");
+
+        for (int i = 0; i <= 17; i = i = i + 2)
+            System.out.println(i);
+
+        System.out.println("Задача 4");
+
+        for (int i = 10; i >= -10; i--)
+            System.out.println(i);
+
+        System.out.println("Задача 5");
+
+        for (int i = 1904; i <= 2096; i = i + 4)
+            System.out.println(i + " год является високосным");
+
+        System.out.println("Задача 6");
+
+        for (int i = 7; i <= 98; i = i + 7)
+            System.out.println(i);
+
+        System.out.println("Задача 7");
+
+        for (int i = 1; i <= 512; i = i * 2)
+            System.out.println(i);
+
+        System.out.println("Задача 8");
+
+        int deposit = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + deposit;
+            System.out.println("Месяц " + i + ",сумма накоплений равна " + total + " рублей");
+        }
+
+        System.out.println("Задача 9");
+
+        int deposit1 = 29000;
+        double total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 = total1 + deposit1 * 1.01;
+            System.out.println("Месяц " + i + ",сумма накоплений равна " + total1 + " рублей");
+        }
+
+        System.out.println("Задача 10");
+
+        int result = 1;
+        for (int i = 1; i <= 10; i++) {
+            result = 2 * i;
+            System.out.println("2*" + i + "=" + result);
         }
     }
 }
